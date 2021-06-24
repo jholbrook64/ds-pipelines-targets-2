fcaf5b9cf43138d78f9afc28b609c730e65f9bf7
Jack Holbrook <jholbrook@usgs.gov>
2021-06-24 08:53:31 -0700

    commit changes to _targets.R to retrun objects, removed funciton 	in get_nwis_data. that concatenated files and instead binds rows 	in targets file. A file is returned and read in 	process_and_style.R script.

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -1,11 +1,13 @@
 #library(Rcpp)
 
-process_data <- function(nwis_data)
+process_data <- function(fileout, nwis_data)
   {
   nwis_data_clean <- rename(nwis_data, water_temperature = X_00010_00000) %>% 
     select(-agency_cd, -X_00010_00000_cd, -tz_cd)
   
-  return(nwis_data_clean)
+    write_csv(nwis_data_clean, fileout)
+  
+  return(fileout) # object
 }
 
 # Rcpp::sourceCpp(code='
@@ -17,17 +19,18 @@ process_data <- function(nwis_data)
 # return(left_joinCpp(site_data_clean, DataFrame site_info, "site_no"));
 # }')
 
-annotate_data <- function(site_data_clean, site_filename)
+annotate_data <- function(site_data_clean, site_filename)  # this should take a filein arg now. 
   {
   site_info <- read_csv(site_filename)
+  site_data_clean <-  read_csv(site_data_clean)
   annotated_data <- left_join(site_data_clean, site_info, "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va)
   
-  return(annotated_data)
+  return(annotated_data) # object
 }
 
 
 style_data <- function(site_data_annotated)
   {
-  mutate(site_data_annotated, station_name = as.factor(station_name))
+  mutate(site_data_annotated, station_name = as.factor(station_name))   # no return 
 }
